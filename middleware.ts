bb184fad54bac17686df9b6103fcd36b2513c8b3
Neil Movva <neilmovva@gmail.com>
2023-10-23 17:20:24 -0700

    fix console protected route

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
-export default authMiddleware({ publicRoutes: ['/'] });
+export default authMiddleware({
+    beforeAuth: (req) => {
+        if (!req.url.endsWith("/console")) {
+            return false;
+        }
+
+        return redirectToSignIn();
+    },
+
+    // Ensure that locale specific sign-in pages are public
+    publicRoutes: ["/", "/:locale/sign-in"],
+});
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(console)"],
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-
